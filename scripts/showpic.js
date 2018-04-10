9cdf4c791163a1b7865f9b28b1eab3ef3ed66151
Ygria <906078571@qq.com>
2018-04-10 15:48:02 +0800

    改进Js

--- scripts/showpic.js
+++ scripts/showpic.js
@@ -13,4 +13,28 @@ function countBodyChildren(){
     alert(body_element.childNodes.length);
 }
 
+function prepareGallery(){
+    //实现平稳退化：当获取不到tagName时，说明浏览器不支持js
+    console.log("hhh");
+    if(!document.getElementsByTagName)return false;
+    if(!document.getElementById)return false;
+    if(!document.getElementById("imagegallery"))return false;
+
+    console.log("hhh");
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0;i<links.length;i++){
+        links[i].onclick = function(){
+            showPic(this);
+            return false;
+        }
+    }
+}
+
+//这种写法可以在页面加载时
+window.onload = function(){
+    console.log("window onload");
+    prepareGallery();
+}
+
 //window.onload = countBodyChildren;
